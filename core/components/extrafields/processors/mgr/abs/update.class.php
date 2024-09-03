a6d8d20c760bbd1c268363598917e63047ea6de1
Boshnik <Superboshnik@gmail.com>
2024-09-03 16:42:03 +0300

    fixed abs update

--- core/components/extrafields/processors/mgr/abs/update.class.php
+++ core/components/extrafields/processors/mgr/abs/update.class.php
@@ -5,31 +5,16 @@ class efFieldAbsUpdateProcessor extends modObjectUpdateProcessor
     public $classKey = efFieldAbs::class;
     public $objectType = 'ef_field';
     public $languageTopics = ['extrafields'];
-    //public $permission = 'save';
-
-
-    /**
-     * @return bool|null|string
-     */
-    public function initialize()
-    {
-        if (!$this->checkPermissions()) {
-            return $this->modx->lexicon('access_denied');
-        }
-
-        return parent::initialize();
-    }
-
 
     /**
      * @return bool
      */
     public function beforeSet()
     {
-        if ($this->properties['ab_templates']) {
+        if (!empty($this->properties['ab_templates'])) {
             $this->properties['ab_templates'] = implode('||', $this->properties['ab_templates']);
         }
-        if ($this->properties['ab_user_group']) {
+        if (!empty($this->properties['ab_user_group'])) {
             $this->properties['ab_user_group'] = implode('||', $this->properties['ab_user_group']);
         }
 
