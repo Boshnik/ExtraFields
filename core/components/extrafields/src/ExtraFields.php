b45ba3f10d1dfe83bd9e49c7faac56a167a99a90
Boshnik <Superboshnik@gmail.com>
2025-03-04 11:54:59 +0200

    Version 2.0.6

--- core/components/extrafields/src/ExtraFields.php
+++ core/components/extrafields/src/ExtraFields.php
@@ -44,7 +44,7 @@ class ExtraFields
      * The version
      * @var string $version
      */
-    public $version = '2.0.5-pl';
+    public $version = '2.0.6-pl';
 
     /**
      * The class config
