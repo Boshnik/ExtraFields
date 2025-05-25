fb842686ea34939f7cac96406a14c551a399f54c
Boshnik <Superboshnik@gmail.com>
2025-05-25 22:07:46 +0300

    Version 2.0.7

--- core/components/extrafields/src/ExtraFields.php
+++ core/components/extrafields/src/ExtraFields.php
@@ -44,7 +44,7 @@ class ExtraFields
      * The version
      * @var string $version
      */
-    public $version = '2.0.6-pl';
+    public $version = '2.0.7-pl';
 
     /**
      * The class config
